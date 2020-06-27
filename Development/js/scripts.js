44a31c1fb82af69e30e976f115e6c71f38271c0b
Fatmah-alrashidi-481 <66441802+Fatmah-alrashidi-481@users.noreply.github.com>
2020-06-27 20:39:37 +0300

    done

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,27 +1,33 @@
 // السؤال الأول
 // 1
+const name  = "fatmah";
+
 
 // 2
-
+let number = 16;
 // 3 and 4
+console. log(name + " " + number)
 
 // 5 and 6
-
+console.log (number *5)
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+const hobies = ["swimming ","basketball" ,"football"]
 // 2
-
+hobies.push ("basketball")
 // 3
 
 // 4
-
+const fatmah = {
+    trsck :"web" ,
+    lang : "html" ,
+} ;
 // 5
 
 // 6
-
+console.log (fatmah.trsck)
 // 7
 
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
